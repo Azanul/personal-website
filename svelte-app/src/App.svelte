b6646d1aa38456a90439f9ca011892f082f74637
Azanul <azanulhaque@gmail.com>
2022-04-27 19:45:27 +0000

    Basic navbar with sidebar

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,5 +1,21 @@
 <script>
-	let name = 'world';
+	import Navbar from '../components/Navbar.svelte'
+	import Sidebar from '../components/Sidebar.svelte'
+	import Main from '../components/Main.svelte'
+
+	let open = false
 </script>
 
-<h1>Hello {name}!</h1>
+<Sidebar bind:open/>
+<Navbar bind:sidebar={open}/>
+<Main/>
+
+<svelte:head>
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
+</svelte:head>
+
+<style>
+	:global(body) {
+		padding: 0;
+	}
+</style>
---
+++ svelte-app/src/Menu.svelte
@@ -0,0 +1,4 @@
+<nav class="hidden text-gray-500 uppercase text-bold sm:flex sm:justify-center space-x-4">
+    <a href="/about" class="hover:text-gray-700 hover:no-underline">About</a>
+      <a href="/contact" class="hover:text-gray-700 hover:no-underline">Contact</a>
+  </nav>
