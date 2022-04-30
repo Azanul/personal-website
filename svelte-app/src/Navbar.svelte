b5771693060874055c07041c62b288f5b1482e3a
Azanul <azanulhaque@gmail.com>
2022-04-30 20:38:58 +0000

    navbar style UPDATE

--- svelte-app/src/Navbar.svelte
+++ svelte-app/src/Navbar.svelte
@@ -6,7 +6,7 @@
 	export let sidebar = false
 </script>
 
-<header class="flex justify-between bg-gray-200 p-2 items-center text-gray-600 border-b-2">
+<header class="flex justify-between bg-transparent p-2 items-center text-gray-600 border-b-2">
 	<nav class="flex">
 		<Hamburger bind:open={sidebar}/>
 		<Logo/>	
