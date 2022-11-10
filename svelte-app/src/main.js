39b8f4dbcf51a68436753a0b094041eaedb55a99
Azanul <azanulhaque@gmail.com>
2022-11-10 17:30:14 +0000

    wasm disabled

--- svelte-app/src/main.js
+++ svelte-app/src/main.js
@@ -4,17 +4,17 @@ import App from './App.svelte';
 // The wasm-plugin runs `wasm-pack build` and cpoies the output into
 // `svelte-app/target` directory.
 // The `.wasm` file is located in the `svelte-app/public/build` dir.
-import wasm from '../../wasm-game-of-life/Cargo.toml';
+// import wasm from '../../wasm-game-of-life/Cargo.toml';
 
 // WebAssembly files must be loaded async.
 const init = async () => {
-    const gameOfLife = await wasm();
+    // const gameOfLife = await wasm();
 
     const app = new App({
         target: document.body,
         props: {
           // https://svelte.dev/docs#Creating_a_component
-          greet: gameOfLife.greet()
+          // greet: gameOfLife.greet()
         }
     });
 };
