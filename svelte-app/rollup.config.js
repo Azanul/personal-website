29628633dffdbbce63c0fb9c0b0d5f34b097d4f2
Azanul <azanulhaque@gmail.com>
2022-11-12 11:27:55 +0000

    env reading

--- svelte-app/rollup.config.js
+++ svelte-app/rollup.config.js
@@ -5,6 +5,8 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 import rust from "@wasm-tool/rollup-plugin-rust";
+import {config} from 'dotenv';
+import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -40,6 +42,16 @@ export default {
   		dir: "public/build"
 	},
 	plugins: [
+		replace({
+			// stringify the object       
+			__myapp: JSON.stringify({
+			  env: {
+				isProd: production,
+				...config().parsed // attached the .env config
+			  }
+			}),
+		}),
+
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
