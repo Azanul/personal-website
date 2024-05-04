f69985c03e9c605ce2d69b4f5db29b2c36253e0f
Azanul <azanulhaque@gmail.com>
2024-05-04 12:01:50 +0530

    feat: add wheel
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 import CarChassis from './models/car/chassis.glb';
 
+import CarWheel from './models/car/wheel.glb';
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -15,6 +18,15 @@ loader.load( CarChassis, function ( gltf ) {
 }, undefined, function ( error ) {
 	console.error( error );
 } );
+
+var wheel;
+loader.load( CarWheel, function ( gltf ) {
+	wheel = gltf.scene;
+	scene.add( wheel );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+
 scene.background = new THREE.Color( 0xfaa9a9 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
