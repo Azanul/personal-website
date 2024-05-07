1383deb634acb619dd305d8ab55543cb32c027ef
Azanul <azanulhaque@gmail.com>
2024-05-08 04:16:51 +0530

    feat: seperate wheels
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- src/js/car.js
+++ src/js/car.js
@@ -1,5 +1,4 @@
 import * as THREE from "three"
-import { TransformControls } from "three/addons/controls/TransformControls.js"
 
 export default class Car {
     constructor(_options) {
@@ -8,10 +7,41 @@ export default class Car {
 
         this.container = new THREE.Object3D()
 
-        this.chassis = _options.resources.items.carChassis
-        this.wheel = _options.resources.items.carWheel
+        this.chassis = { model: _options.resources.items.carChassis }
+        this.wheel = { model: _options.resources.items.carWheel }
 
-        this.container.add(this.chassis)
-        this.container.add(this.wheel)
+        this.container.add(this.chassis.model)
+
+        this.setWheels()
+    }
+
+    setWheels() {
+        const wheelPositions = [
+            [
+                { x: 0.2, y: 0, z: 0 },
+                { x: 0, y: 3.14159, z: 0 },
+            ], //Right Back
+            [
+                { x: -0.1, y: 0, z: 0 },
+                { x: 0, y: 0, z: 0 },
+            ], //Left Front
+            [
+                { x: 1.2, y: 0, z: 0 },
+                { x: 0, y: 3.14159, z: 0 },
+            ], //Right Front
+            [
+                { x: -1.1, y: 0, z: 0 },
+                { x: 0, y: 0, z: 0 },
+            ], //Left Back
+        ]
+
+        wheelPositions.forEach((posrot) => {
+            const position = posrot[0]
+            const rotation = posrot[1]
+            const wheelMesh = this.wheel.model.clone()
+            wheelMesh.position.set(position.x, position.y, position.z)
+            wheelMesh.rotation.set(rotation.x, rotation.y, rotation.z)
+            this.container.add(wheelMesh)
+        })
     }
 }
--- src/js/physics.js
+++ src/js/physics.js
@@ -37,8 +37,9 @@ export default class Physics {
     }
 
     setCar() {
+        console.log(this.car.wheel)
         this.car.chassis.shape = new CANNON.Box(
-            new CANNON.Vec3(2 * 0.5, 1 * 0.5, 1 * 0.5)
+            new CANNON.Vec3(this.car.chassis * 0.5, 1 * 0.5, 1 * 0.5)
         )
 
         this.car.chassis.body = new CANNON.Body({
