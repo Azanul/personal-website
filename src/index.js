35ca90ff2ea6a6fea34479d15e8f20b015177b9f
Azanul <azanulhaque@gmail.com>
2024-07-03 15:11:27 +0530

    feat: camera handling and alignment with car rotation
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ import Car from "./js/car"
 import Earth from "./js/earth"
 import Physics from "./js/physics"
 
+function clamp(value, min, max) {
+    console.log(min, value, max)
+    return Math.max(min, Math.min(max, value));
+}
+
+const xMin = 10;
+const xMax = 150;
+const yMin = 10;
+const yMax = 150;
+const zMin = 10;
+const zMax = 150;
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
     24,
@@ -22,7 +34,6 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const cameraControls = new TrackballControls(camera, renderer.domElement)
-cameraControls.target.set(0, 0, 0)
 
 const light = new THREE.AmbientLight(0x404040, 10)
 scene.add(light)
@@ -47,18 +58,34 @@ THREE.DefaultLoadingManager.onLoad = function () {
     let physics = new Physics({ car, earth })
     scene.add(physics.container)
 
-	var cannonDebugRenderer = new CannonDebugger( scene, physics.world );
+    var cannonDebugRenderer = new CannonDebugger(scene, physics.world)
+
+    const desiredPosition = new THREE.Vector3()
 
     function animate() {
         requestAnimationFrame(animate)
 
         physics.update()
-        // camera.position.y = car.container.y - 10
+
+        desiredPosition.set(
+            clamp(camera.position.x, car.container.position.x+xMin, car.container.position.x+xMax),
+            clamp(camera.position.y, car.container.position.x+yMin, car.container.position.x+yMax),
+            clamp(camera.position.z, car.container.position.x+zMin, car.container.position.x+zMax)
+        )
+        console.log(camera.position, desiredPosition)
+
+        camera.position.lerp(desiredPosition, 0.1)
 
         camera.lookAt(car.container.position)
+        cameraControls.target.copy(car.container.position)
+
+        const carUp = new THREE.Vector3(0, 1, 0)
+        carUp.applyQuaternion(car.container.quaternion)
+        camera.up.copy(carUp)
+
         cameraControls.update()
 
-		cannonDebugRenderer.update()
+        cannonDebugRenderer.update()
         renderer.render(scene, camera)
     }
 
